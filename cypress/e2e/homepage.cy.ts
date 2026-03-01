55c0bef9da7918178d210b2123edf6f194067eba
ArteaWright <artealwright@gmail.com>
2026-02-28 23:17:35 -0500

    chore: remove unused cypress e2e test code

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -6,14 +6,4 @@ describe('Integration test with visual testing', () => {
   it('Loads the homepage', () => {
     cy.percySnapshot('Homepage Loads');
   });
-
-  it('Has header and paragraph', () => {
-    cy.get('h2')
-      .should('be.visible')
-      .and('contain.text', 'Dashboard');
-    cy.get('p')
-      .should('be.visible')
-      .and('contain.text', 'Component content goes here.');
-    cy.percySnapshot('Homepage components');
-  });
 });
