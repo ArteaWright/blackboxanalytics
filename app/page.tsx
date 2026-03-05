2bbdf12ad05e3245fc67fdbfd83d7cd839117bed
ArteaWright <artealwright@gmail.com>
2026-03-05 01:04:51 -0500

    feat: test vercel deployment pipeline with new release and deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 
-
-import Test from "./components/Test/Test";
 import NewTest from "./components/NewTest/NewTest";export default function Home() {
   return (
     <div className="home_container">
       <NewTest />
-      <Test /></div>
+    </div>
   );
 }
