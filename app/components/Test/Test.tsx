0606e3fd5eacb65850ec26704a988f3f0aea0f4b
ArteaWright <artealwright@gmail.com>
2026-03-04 22:01:14 -0500

    feat: test vercel deployment pipeline

---
+++ app/components/Test/Test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div className="test">
+      <h2>Test</h2>
+      <p>Component content goes here.</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
-export default function Home() {
+
+import Test from "./components/Test/Test";export default function Home() {
   return (
-    <div className="home_container"></div>
+    <div className="home_container">
+      <Test /></div>
   );
 }
